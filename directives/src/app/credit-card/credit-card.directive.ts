cc483aac490d32f9b711827e618ba538c310f7e9
kukori <attila.varjas@gmail.com>
2022-05-07 18:55:00 +0200

    HostListener ad host object

--- directives/src/app/credit-card/credit-card.directive.ts
+++ directives/src/app/credit-card/credit-card.directive.ts
@@ -1,10 +1,24 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[credit-card]'
 })
 export class CreditCardDirective {
-  constructor(private element: ElementRef) {
-    console.log(this.element);
+  // in this case the host is the input element
+  @HostListener('input', ['$event']) 
+  onKeyDown(event: KeyboardEvent) {
+    const input = event.target as HTMLInputElement;
+
+    let trimmed = input.value.replace(/\s+/g, '');
+    if(trimmed.length > 16) {
+      trimmed = trimmed.substr(0, 16);
+    }
+
+    let numbers = [];
+    for(let i = 0; i < trimmed.length; i += 4) {
+      numbers.push(trimmed.substr(i, 4));
+    }
+
+    input.value = numbers.join(' ');
   }
 }
